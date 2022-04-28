49bdd7d338710d065281081b94481fb92026dc4a
Riskibotz1 <103689256+Riskibotz1@users.noreply.github.com>
2022-04-28 08:41:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 global.DeveloperMode = 'true' //true atau false, true berati iya false berati tidak Moga paham
 global.owner = ['6289531724196'] // masuk in no lu sebagai owner
-global.mods = ['6285723813401'] // masuk in no moderator ny
+global.mods = ['6289531724196'] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
   // name: 'https://website'
