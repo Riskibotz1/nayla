8105fb4a67e192eea948d218ca2de1fb65e05b54
Riskibotz1 <103689256+Riskibotz1@users.noreply.github.com>
2022-05-05 13:32:38 +0700

    Update pacarowner.js

--- plugins/pacarowner.js
+++ plugins/pacarowner.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6289531724196', 'Riski Gans', m)
-  conn.sendContact(m.chat, '6285311345084' , 'novi Cans', m)
+  conn.sendContact(m.chat, '6281269361609' , 'kanaya Cans', m)
   conn.reply(m.chat, `Tuh Pacar Ku Jangan Galak² Ya >_<, Nanti Ku Banned Nanges`, m) 
 }
 handler.help = ['pacarowner']
